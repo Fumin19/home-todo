92a83bd5b361fccf5cf219527034cd35a91c9b45
Fumin19 <62929932+Fumin19@users.noreply.github.com>
2022-08-02 12:03:40 +0200

    added a bit of SCSS

--- Frontend/src/app/services/toDo/to-do.service.ts
+++ Frontend/src/app/services/toDo/to-do.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ToDo } from 'src/app/models/models';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ToDoService {
   private toDos: ToDo[] = [];
