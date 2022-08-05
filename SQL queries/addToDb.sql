13543cedf47b839ed94780da62de6841dc9f6aa5
Fumin19 <62929932+Fumin19@users.noreply.github.com>
2022-08-05 11:40:41 +0200

    Delete SQL queries directory

--- SQL queries/addToDb.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO toDos (text, isFinished)
-VALUES 
-("first todo", 0),
-("second todo", 0),
-("second todo", 0),
-("second todo", 0);
--- SQL queries/createToDoTable.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE toDos (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  text varchar(50),
-  isFinished tinyint(1),
-  PRIMARY KEY (id)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=5 ;
